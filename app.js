7cf20de228a02edd38600c1a9399b48a84e55ba8
GhosTHaise <Ghostrex2@gmail.com>
2021-09-28 10:47:18 +0300

    Changement decisif 2

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ const res = require("express/lib/response")
 const PORT = process.env.PORT || 3000
 
 app.get("",(req,res) => {
-<<<<<<< HEAD
     res.send("<h1>I am the GhosT</h1>");
-=======
-    res.send("<h1>Fitiavana Sambatra<h1>");
->>>>>>> 81e381c12c67bd1b8e539ce31dfc8bdc73bb80c6
 });
 
 app.listen(PORT,() => {
